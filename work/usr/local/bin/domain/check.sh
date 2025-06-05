7306c0a15324bee06fd90654f2f03e1c1ea0eec0
Jim <blyzzcanada@gmail.com>
2025-06-05 16:59:34 -0700

    Update check.sh

--- work/usr/local/bin/domain/check.sh
+++ work/usr/local/bin/domain/check.sh
@@ -1,23 +1,27 @@
 #! /bin/bash
 
+INPUT=$1
+
 [[ -z $1 ]] && echo -e "\n  Usage: domain [OPTION]...\n  Where: [OPTION] is a domain name - example: blyzz.com\nExample: domain blyzz.com\n" && exit 1
 
-DNAME=$(echo $1 | awk -F'.' '{print $1}')
-DTLD=$(echo $1 | awk -F'.' '{$1="";print $0}' | cut -c2-)
+INPUT="${INPUT##*@}"
+INPUT="${INPUT#*://}"
+INPUT="${INPUT%%/*}"
+INPUT="${INPUT%%:*}"
+DOMAIN=$(echo "$INPUT" | awk -F. '{if (NF>1) print $(NF-1)"."$NF; else print $0}')
+DNAME=$(echo $DOMAIN | awk -F'.' '{print $1}')
+DTLD=$(echo $DOMAIN | awk -F'.' '{$1="";print $0}' | cut -c2-)
 
 DPATH=/usr/local/bin/domain
 
-wget -q -O $DPATH/$1.whois https://www.whois.com/whois/$1
+wget -q -O $DPATH/$DOMAIN.whois https://www.whois.com/whois/$DOMAIN
 
-date > $DPATH/$1.out
-#echo "DNAME=$DNAME"
-#echo "DTLD=$DTLD"
-echo "Domain: $DNAME[.]$DTLD"  >> $DPATH/$1.out
-grep -Eo 'Registered On.{39}' $DPATH/$1.whois | sed 's/<\/div><div class="df-value">/ /' >> $DPATH/$1.out
-grep -Eo 'Updated On.{39}' $DPATH/$1.whois | sed 's/<\/div><div class="df-value">/ /' >> $DPATH/$1.out
-grep -Eo 'Registrar:.{28}[a-ZA-Z0-9\. ]*' $DPATH/$1.whois | sed 's/<\/div><div class="df-value">/ /' >> $DPATH/$1.out
-#grep -Eo 'Status:.{160}' $DPATH/$1.whois  | sed 's/<\/div><div class="df-value">/ /' | awk -F'</div>' '{print $2}' >> $DPATH/$1.out
-grep -Eo 'Status:.{260}' $DPATH/$1.whois | sed 's/<\/div><div class="df-value">/ /' | sed 's/<\/div>/=/' | awk -F'=' '{print $1}' | sed 's/<br>/\n  /g' | sed 's/: /:\n  /' >> $DPATH/$1.out
-rm $DPATH/$1.whois
+date > $DPATH/$DOMAIN.out
+echo "Domain: $DNAME[.]$DTLD"  >> $DPATH/$DOMAIN.out
+grep -Eo 'Registered On.{39}' $DPATH/$DOMAIN.whois | sed 's/<\/div><div class="df-value">/ /' >> $DPATH/$DOMAIN.out
+grep -Eo 'Updated On.{39}' $DPATH/$DOMAIN.whois | sed 's/<\/div><div class="df-value">/ /' >> $DPATH/$DOMAIN.out
+grep -Eo 'Registrar:.{28}[a-ZA-Z0-9\. ]*' $DPATH/$DOMAIN.whois | sed 's/<\/div><div class="df-value">/ /' >> $DPATH/$DOMAIN.out
+grep -Eo 'Status:.{260}' $DPATH/$DOMAIN.whois | sed 's/<\/div><div class="df-value">/ /' | sed 's/<\/div>/=/' | awk -F'=' '{print $1}' | sed 's/<br>/\n  /g' | sed 's/: /:\n  /' >> $DPATH/$DOMAIN.out
+rm $DPATH/$DOMAIN.whois
 
-cat $DPATH/$1.out
+cat $DPATH/$DOMAIN.out
