bb15017b7f7e245ddfb279aaf3490caf689b3b1c
Jim <blyzzcanada@gmail.com>
2025-06-12 12:20:32 -0700

    Update init.sh

--- work/usr/local/bin/domain/init.sh
+++ work/usr/local/bin/domain/init.sh
@@ -2,7 +2,7 @@
 
 INPUT="$@"
 CURRENT=$(< /usr/local/bin/domain/check.ver)
-LATEST=$(curl -s https://raw.githubusercontent.com/Blyzz616/Linux-Config/main/work/usr/local/bin/domain/check.ver)
+LATEST=$(curl -s -H "Cache-Control: no-cache" https://raw.githubusercontent.com/Blyzz616/Linux-Config/main/work/usr/local/bin/domain/check.ver)
 
 if [[ $CURRENT -lt $LATEST ]]; then
     curl -s https://raw.githubusercontent.com/Blyzz616/Linux-Config/main/work/usr/local/bin/domain/check.sh > /usr/local/bin/domain/check.sh
