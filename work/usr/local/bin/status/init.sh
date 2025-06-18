023a926e7852e841220648d75a47cbbbf498f870
Jim <blyzzcanada@gmail.com>
2025-06-18 10:09:06 -0700

    Create init.sh

---
+++ work/usr/local/bin/status/init.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+CURRENT=$(< /usr/local/bin/status/scrape.ver)
+LATEST=$(curl -s -H "Cache-Control: no-cache" https://raw.githubusercontent.com/Blyzz616/Linux-Config/main/work/usr/local/bin/status/scrape.ver)
+
+if [[ $CURRENT -lt $LATEST ]]; then
+    curl -s https://raw.githubusercontent.com/Blyzz616/Linux-Config/main/work/usr/local/bin/status/scrape.sh > /usr/local/bin/status/scrape.sh
+fi
+
+/usr/local/bin/status/scrape.sh
